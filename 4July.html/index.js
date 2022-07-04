b57ab84d9d9784eb0e511cbb535a6d3bc27d928d
sneha shinde <Sneha06032000@gmail.com>
2022-07-04 18:05:26 +0530

    output

--- 4July.html/index.js
+++ 4July.html/index.js
@@ -1,23 +1,23 @@
 
     
-    // for(let row=1; row<=5; row++) {
-    //   let stars = '';
-    //   for(let col=row; col<=5; col++) {
-    //     stars += col;
-    //   }
-    //   for(let i=1; i<row; i++) {
-    //     stars += i;
-    //   }
-    //   console.log(stars);
-    // }
-  
-let stars = "";
-for(let row = 1; row <= 6 ; row++){
-  
-    if(row%2 == 0){
-      stars = "0" + stars 
-    }else{
-      stars = "1" + stars 
+    for(let row=1; row<=5; row++) {
+      let stars = '';
+      for(let col=row; col<=5; col++) {
+        stars += col;
+      }
+      for(let i=1; i<row; i++) {
+        stars += i;
+      }
+      console.log(stars);
     }
-  console.log(stars);
-}
+  
+// let stars = "";
+// for(let row = 1; row <= 6 ; row++){
+  
+//     if(row%2 == 0){
+//       stars = "0" + stars 
+//     }else{
+//       stars = "1" + stars 
+//     }
+//   console.log(stars);
+// }
---
+++ 4July.html/readme.md
@@ -0,0 +1,2 @@
+<img src="pattern 1.PNG" alt="output">
+<img src="pattern2.PNG" alt="output">
