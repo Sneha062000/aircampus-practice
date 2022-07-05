ccc20114962e1994ce499f7cf0bf03546def6e22
sneha shinde <Sneha06032000@gmail.com>
2022-07-05 19:10:51 +0530

    my family

--- 5July.html/index.js
+++ 5July.html/index.js
@@ -1,4 +1,23 @@
-let num ="10";
-console.log(num);
-num = 'sneha';
-console.log(num);
+// let num ="10";
+// console.log(num);
+// num = 'sneha';
+// //sneha string data type
+// console.log(num);
+// //js is basically increasing and decreasing memory spaces
+
+//how to store all the peoples name
+let names = ['sneha','shinde','sneha','shinde']
+//all the arrays are named in plural
+
+
+//sneha family
+let snehafamily={
+    name :'sneha',
+    age:21,
+    location:'Nashik',
+ brother:{
+    name:'Rajesh',
+    age:19
+ }
+}
+console.log(snehafamily);
