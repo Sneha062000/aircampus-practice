96bd3bfb5dac37405f007d9b0e039dff3de6d7ae
sneha shinde <Sneha06032000@gmail.com>
2022-07-06 19:11:21 +0530

    push pop

--- 6July.html/index.js
+++ 6July.html/index.js
@@ -1,18 +1,18 @@
- let num = [1,2,3,4,5]
-  //print first element and last element
+//  let num = [1,2,3,4,5]
+//   //print first element and last element
 
-  console.log(num[0], num[4]); 
-
-
+//   console.log(num[0], num[4]); 
 
 
 
 
-//  let num = [1,2,3,4,5,6,7]
 
-//  print first element and last element
 
-// console.log(num[0], num[num.length -1]);
+//   let num = [1,2,3,4,5,6,7]
+
+// //  print first element and last element
+
+//  console.log(num[0], num[num.length -1]);
  
 
 
@@ -20,21 +20,21 @@
 
 
 //.................stack in arrays
-// let num = [1,2,3,4,5]
+let num = [1,2,3,4,5]
 
-//  console.log(num);
+ console.log(num);
 
-//  num.push('sneha')
+ num.push('sneha')
 
-//  console.log('adding', num);
+ console.log('adding', num);
 
-// console.log(num.pop());
+console.log(num.pop());
 
-// console.log('popping',num);
+console.log('popping',num);
 
-// console.log(num.pop());
+console.log(num.pop());
 
-// console.log('popping',num);
+console.log('popping',num);
 
 
 
--- 6July.html/readme.md
+++ 6July.html/readme.md
@@ -1,1 +1,2 @@
 <img src="first and last num.PNG" alt="output">
+<img src="push and pop.PNG" alt="output">
