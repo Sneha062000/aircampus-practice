0325c081274be94e8eec047642312e688507858b
sneha shinde <Sneha06032000@gmail.com>
2022-07-07 22:51:53 +0530

    output

--- 7July.html/index.js
+++ 7July.html/index.js
@@ -1,37 +1,37 @@
-// function aircampus(n){
-//     let original = n, count = 0, sum = 0;
-//     let str = '';
-//       while(n!=0){
-//         n = Math.floor(n/10);
-//         count++;
-//       }
-//       console.log(n);
-//       n= original; 
-//       while(n > 0) {
-//         let digit = n % 10;
-//         n = Math.floor(n/10);
-//         sum += digit ** count;
+function aircampus(n){
+    let original = n, count = 0, sum = 0;
+    let str = '';
+      while(n!=0){
+        n = Math.floor(n/10);
+        count++;
+      }
+      console.log(n);
+      n= original; 
+      while(n > 0) {
+        let digit = n % 10;
+        n = Math.floor(n/10);
+        sum += digit ** count;
         
-//       }
-//       console.log(n);
-//       if(str == ''){
-//         return '-1';
-//       }else{
-//         return str;
-//       }
-//     }
+      }
+      console.log(n);
+      if(str == ''){
+        return '-1';
+      }else{
+        return str;
+      }
+    }
     
-//     console.log(aircampus(123));
+    console.log(aircampus(123));
     
 
-let n = 6;
-let str = "";
-for(let i = 1; i <= n ; i++){
+// let n = 6;
+// let str = "";
+// for(let i = 1; i <= n ; i++){
   
-    if(i%2 == 0){
-      str = "0" + str 
-    }else{
-      str = "1" + str 
-    }
-  console.log(str);
-}
+//     if(i%2 == 0){
+//       str = "0" + str 
+//     }else{
+//       str = "1" + str 
+//     }
+//   console.log(str);
+// }
---
+++ 7July.html/readme.md
@@ -0,0 +1,2 @@
+<img src="output 1.PNG" alt="output">
+<img src="output 2.PNG" alt="output">
