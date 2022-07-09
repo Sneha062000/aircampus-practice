1cac9a55c640bfd538c7fca51fb309545221fca9
sneha shinde <Sneha06032000@gmail.com>
2022-07-09 23:21:17 +0530

    replit que

---
+++ 9July.html/index.js
@@ -0,0 +1,12 @@
+ //function createArray(){
+     let num = ['Bike','Scooter','Car','Truck','JCB','Helicopter'];
+
+     //return num;
+//}
+      console.log(num[3]);
+ 
+        //  let num = [1,2,3,4,5]
+        //  //   //print first element and last element
+         
+        //     console.log(num[0], num[4]); 
+         
