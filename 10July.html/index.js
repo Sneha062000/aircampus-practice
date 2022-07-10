c0cb4c542666ca4c9921b33572756332f635f220
sneha shinde <Sneha06032000@gmail.com>
2022-07-10 22:43:47 +0530

    join

--- 10July.html/index.js
+++ 10July.html/index.js
@@ -8,6 +8,23 @@
 // console.log(nums);
 
 
-let nums=[1,23,12,45,35,56,76,78,80];
-nums.sort((a,b)=>a-b);
-console.log(nums[nums.length-1]);
+// let nums=[1,23,12,45,35,56,76,78,80];
+// nums.sort((a,b)=>a-b);
+// console.log(nums[nums.length-1]);
+
+
+// let nums=[-1,-23,-45,-35,-56,-76,-78,-80];
+// let max = nums[0];
+// for(let i=1;i=nums.length;i++){
+//     let currentValue = nums[i];
+//     if(max < currentValue){
+//         max = currentValue;
+//     }
+// }
+// console.log(max);
+
+
+
+let names=['sneha','manoj','shinde'];
+
+console.log(names.join('------'));
--- 10July.html/readme.md
+++ 10July.html/readme.md
@@ -1,3 +1,4 @@
 <img src="sort output1.PNG" alt="output">
 <img src="sort output2.PNG" alt="output">
 <img src="largest no.PNG" alt="output">
+<img src="join output.PNG" alt="output">
