939c2778280acf117684b2d6869f68abb101c88d
sneha shinde <Sneha06032000@gmail.com>
2022-07-11 18:39:52 +0530

    push and pop

--- 11July.html/index.js
+++ 11July.html/index.js
@@ -37,14 +37,33 @@
 // console.log(min);
 
 
-let nums=[1,20,32,17,89,10,27,100];
-let min = 0;
-for(i=0;i-nums.length;i++){
-    let currentValue=nums[i];
-    if(min < currentValue){
-        min = currentValue
-    }
-}
+// let nums=[1,20,32,17,89,10,27,100];
+// let min = 0;
+// for(i=0;i-nums.length;i++){
+//     let currentValue=nums[i];
+//     if(min < currentValue){
+//         min = currentValue
+//     }
+// }
 
-console.log(min);
+// console.log(min);
+
+
+let num = [1,2,3,4,5,6,7,8];
+
+ console.log(num);
+
+ num.push('sneha')
+
+ console.log('adding', num);
+
+console.log(num.pop());
+
+console.log('popping',num);
+
+console.log(num.pop());
+
+console.log('popping',num);
+
+
 
