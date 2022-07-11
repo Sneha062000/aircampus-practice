0d009e25f0fbdf7acad882054ba64ba7a1e2750e
sneha shinde <Sneha06032000@gmail.com>
2022-07-11 18:30:28 +0530

    min with negative no

--- 11July.html/index.js
+++ 11July.html/index.js
@@ -12,13 +12,27 @@
 
 
 
-let nums=[1,20,32,17,89,10,27,100];
-let max = 1;
-for(i=1;i<nums.length;i++){
+// let nums=[1,20,32,17,89,10,27,100];
+// let max = 1;
+// for(i=1;i<nums.length;i++){
+//     let currentValue=nums[i];
+//     if(max < currentValue){
+//         max = currentValue
+//     }
+// }
+
+// console.log(max);
+
+
+
+let nums=[-1,-20,-32,-17,-89,-10,-27,-100];
+let min = 0;
+for(i=1;i-nums.length;i++){
     let currentValue=nums[i];
-    if(max < currentValue){
-        max = currentValue
+    if(min > currentValue){
+        min = currentValue
     }
 }
 
-console.log(max);
+console.log(min);
+
