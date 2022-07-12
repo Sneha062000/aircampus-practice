7313daddd59f5130984933ea53f0db948fa711fa
sneha shinde <Sneha06032000@gmail.com>
2022-07-12 22:41:00 +0530

    planets que

--- 12July.html/index.js
+++ 12July.html/index.js
@@ -1,11 +1,20 @@
-function oddOne(arr1, arr2){
-    let a=arr1.concat(arr2);
-      let b  = a.filter((num)=>{
-        return num%2==0;
-      })
-      return b.sort((c,d)=>{
-        return d-c;
-      });
-      
+// function oddOne(arr1, arr2){
+//     let a=arr1.concat(arr2);
+//       let b  = a.filter((num)=>{
+//         return num%2==0;
+//       })
+//       return b.sort((c,d)=>{
+//         return d-c;
+//       });
+
+//     }
+//     console.log(oddOne([1,2,3,4,5],[1,2,3]));
+
+function lostPlanet(planets){
+    for(let i=0;i<planets.length;i++){
+      if(planets.includes(i)==false){
+        return i;
+      }
     }
-    console.log(oddOne([1,2,3,4,5],[1,2,3]));
+  }
+  console.log(lostPlanet([1,5,7,8,3,0,2,6]));
