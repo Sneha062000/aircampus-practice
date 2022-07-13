6152c9693e5772369b6af5b1e1567cf0be7d4dbd
sneha shinde <Sneha06032000@gmail.com>
2022-07-13 22:20:53 +0530

    2D array

--- 13July.html/index.js
+++ 13July.html/index.js
@@ -7,3 +7,22 @@
 // let nums1=[1,2,3]
 // let nums2=[4,5,6]
 // console.log(`${nums1},${nums2}`)
+
+
+
+let matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+let rows = matrix.length;
+let cols = matrix[0].length;
+
+let sum = 0;
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        sum += matrix[i][j];
+    }
+}
+console.log(sum);
+
