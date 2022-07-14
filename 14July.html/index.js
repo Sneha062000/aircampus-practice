59b2a9bc6eac0f4d6198c1021bc429be860808e5
sneha shinde <Sneha06032000@gmail.com>
2022-07-14 18:16:09 +0530

    without any method

--- 14July.html/index.js
+++ 14July.html/index.js
@@ -1,19 +1,19 @@
-let nums1=[1,2,3];
-let nums2=[4,5,6];
-console.log(nums1+","+nums2);
+// let nums1=[1,2,3];
+// let nums2=[4,5,6];
+// console.log(nums1+","+nums2);
 
 // let nums1=[1,2,3]
 // let nums2=[4,5,6]
 // console.log(`${nums1},${nums2}`)
 
 
-// let nums1=[1,2,3];
-// let nums2=[4,5,6];
-// let nums3=[];
-// for(let i=0;i<nums1.length;i++){
-//     nums3[i]=nums1[i];
-// }
-// for(let i=0;i<nums2.length;i++){
-//     nums3[i+nums2.length]=nums2[i]
-// }
-// console.log(nums3);
+let nums1=[1,2,3];
+let nums2=[4,5,6];
+let nums3=[];
+for(let i=0;i<nums1.length;i++){
+    nums3[i]=nums1[i];
+}
+for(let i=0;i<nums2.length;i++){
+    nums3[i+nums2.length]=nums2[i]
+}
+console.log(nums3);
