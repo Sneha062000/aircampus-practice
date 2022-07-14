99c9114759488fae0c264fc5876e99d47e553b3c
sneha shinde <Sneha06032000@gmail.com>
2022-07-14 18:14:30 +0530

    without any method

--- 13July.html/index.js
+++ 13July.html/index.js
@@ -27,21 +27,23 @@
 // console.log(sum);
 
 
-let matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10,11,12],
-    [13,14,15,16]
-]
-let rows = matrix.length;
-let start = matrix[0]. length -1;
+// let matrix = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10,11,12],
+//     [13,14,15,16]
+// ]
+// let rows = matrix.length;
+// let start = matrix[0]. length -1;
 
-let sum = 0;
-for (let i = 0; i < rows; i++) {
-    sum += matrix [i] [start]
-    start -= 1;
-}
-console.log(sum);
+// let sum = 0;
+// for (let i = 0; i < rows; i++) {
+//     sum += matrix [i] [start]
+//     start -= 1;
+// }
+// console.log(sum);
+
+
 
 
 
