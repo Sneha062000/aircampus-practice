2430a073d8fac59d703bb47b9da6e51dc4876fa1
sneha shinde <Sneha06032000@gmail.com>
2022-07-14 22:44:31 +0530

    sum

--- 14July.html/index.js
+++ 14July.html/index.js
@@ -91,35 +91,35 @@
 
 
 
-let matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-]
-let rows=matrix.length;
-let cols=matrix[0].length;
-console.log(rows,cols);
-
-
-
-
 // let matrix = [
 //     [1, 2, 3, 4],
 //     [5, 6, 7, 8],
-//     [9, 10,11,12],
-//     [13,14,15,16]
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
 // ]
-// let rows = matrix.length;
-// let cols = matrix[0].length;
+// let rows=matrix.length;
+// let cols=matrix[0].length;
+// console.log(rows,cols);
 
-// let sum = 0;
-// for (let i = 0; i < rows; i++) {
-//     for (let j = 0; j < cols; j++) {
-//         sum += matrix[i][j];
-//     }
-// }
-// console.log(sum);
+
+
+
+let matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10,11,12],
+    [13,14,15,16]
+]
+let rows = matrix.length;
+let cols = matrix[0].length;
+
+let sum = 0;
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        sum += matrix[i][j];
+    }
+}
+console.log(sum);
 
 // first col
 // let matrix = [
