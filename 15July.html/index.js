6d56f00fe5c1e484854762a1618c2d788c66676e
sneha shinde <Sneha06032000@gmail.com>
2022-07-15 20:45:15 +0530

    target

--- 15July.html/index.js
+++ 15July.html/index.js
@@ -1,2 +1,8 @@
-let names ='sneha manoj shinde';
- console.log(names.substring(5,11));
+// let names ='sneha manoj shinde';
+//  console.log(names.substring(5,11));
+
+
+
+let news = "hello sneha";
+let target = "sneha";
+console.log(news.includes(target));
