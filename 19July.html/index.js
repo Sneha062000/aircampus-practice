0de3388e7100c910468634aeac38c8db836af176
sneha shinde <Sneha06032000@gmail.com>
2022-07-19 22:25:28 +0530

    tostring

--- 19July.html/index.js
+++ 19July.html/index.js
@@ -1,4 +1,9 @@
-let names = ["Sneha", "Karishma", "Priya", "Tejal"];
+// let names = ["Sneha", "Karishma", "Priya", "Tejal"];
 
-let sortedArray = names.sort();
-console.log(sortedArray);
+// let sortedArray = names.sort();
+// console.log(sortedArray);
+
+let items = ["sneha", 1, "shinde", 2];
+let itemsString = items.toString();
+
+console.log(itemsString);
