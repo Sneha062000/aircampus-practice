c68854080891100527d16ad58fbe97a28889cc52
sneha shinde <Sneha06032000@gmail.com>
2022-07-20 18:47:58 +0530

    factory method

--- 20July.html/objects.js
+++ 20July.html/objects.js
@@ -1,11 +1,25 @@
-function createPerson(fullName,job,place){
-   return {
-        fullName:fullName,
-        job  : job,
-        place : place,
+// function createPerson(fullName,job,place){
+//    return {
+//         fullName:fullName,
+//         job  : job,
+//         place : place,
 
-    };
+//     };
+// }
+// let sneha =  createPerson('sneha','student','Nashik');
+// let karishma =  createPerson('karishma','student','Nashik');
+// console.log(sneha,karishma.job);s
+
+
+
+//factory method
+function createCircle(radius = 1){
+   return {
+        radius: radius,
+        draw: function(){
+           return "drawing circle"
+        },
+    }
 }
-let sneha =  createPerson('sneha','student','Nashik');
-let karishma =  createPerson('karishma','student','Nashik');
-console.log(sneha,karishma.job);
+let circle =  createCircle(10)
+console.log(circle.radius,circle.draw(),circle)
