a7a1b6d307e5acda07c83b522865975cb84d9856
sneha shinde <Sneha06032000@gmail.com>
2022-07-20 19:08:01 +0530

    outputs

--- 20July.html/objects.js
+++ 20July.html/objects.js
@@ -1,14 +1,14 @@
-// function createPerson(fullName,job,place){
-//    return {
-//         fullName:fullName,
-//         job  : job,
-//         place : place,
+function createPerson(fullName,job,place){
+   return {
+        fullName:fullName,
+        job  : job,
+        place : place,
 
-//     };
-// }
-// let sneha =  createPerson('sneha','student','Nashik');
-// let karishma =  createPerson('karishma','student','Nashik');
-// console.log(sneha,karishma.job);s
+    };
+}
+let sneha =  createPerson('sneha','student','Nashik');
+let karishma =  createPerson('karishma','student','Nashik');
+console.log(sneha,karishma.job);
 
 
 
@@ -26,16 +26,16 @@
 
 
 //constructor method
-function Circle(radius){
-   // console.log("before setting values",this);
-    this.radius=radius;
-    this.draw=function(){
-        return "drawing circle";
-    };
-    return this;
-}
-let circle =  new Circle(1);
-console.log(circle,circle.draw());
+// function Circle(radius){
+//    // console.log("before setting values",this);
+//     this.radius=radius;
+//     this.draw=function(){
+//         return "drawing circle";
+//     };
+//     return this;
+// }
+// let circle =  new Circle(1);
+// console.log(circle,circle.draw());
 
-let circle1 =  new Circle(200);
-console.log(circle1,circle.draw());
+// let circle1 =  new Circle(200);
+// console.log(circle1,circle.draw());
---
+++ 20July.html/readme.md
@@ -0,0 +1,3 @@
+<img src="first output.PNG" alt="output">
+<img src="second output.PNG" alt="output">
+<img src="third output.PNG" alt="output">
