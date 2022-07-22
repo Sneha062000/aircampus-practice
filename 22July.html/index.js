dfc7263efdfce1ae11ef22e920f058e825534f43
sneha shinde <Sneha06032000@gmail.com>
2022-07-22 18:57:09 +0530

    javascript pratice

---
+++ 22July.html/index.js
@@ -0,0 +1,78 @@
+// //Simple Addition 
+// let a=5;
+// let b=6;
+// c= a+b;
+// console.log(c);
+
+
+// // numbers from 1-10
+// for(i=1; i<=10;i++){
+//     console.log(i);
+// }
+
+
+//Fahrenheit to Celsius
+// var F=77;
+// var C=(F-32)*(5/9);
+// console.log("Fahrenheit to celsius",C);
+
+
+//Cube of a number//
+// let a1=2;
+// let cube=(a1*a1*a1);
+// console.log("Cube of 5 is",cube);
+
+// // Addition assignment operator
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+// //The sum of 2 and 3 
+// let a = 2; 
+// let b = 3;
+// let c = a + b;
+// console.log("The Sum of " + a + " and " + b + " is: " + c);
+
+
+// //for loop
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+// }
+
+
+// //sum of natural numbers
+// let sum = 0;
+// const n = 10
+// for (let i = 1; i <= n; i++) {
+//     sum += i;  
+// }
+// console.log(sum)
+
+
+// //while loop
+
+// let i = 1, n = 5;
+// while (i <= n) {
+//     console.log(i);
+//     i += 1;
+// }
+
+
+// //do while loop
+// let i2 = 1;
+// const n3 = 5;
+// do {
+//     console.log(i2);
+//     i2++;
+// } while(i2 <= n3)
+
+
+
+// //do while loop
+// let i = 1;
+// const n = 5;
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= n)
