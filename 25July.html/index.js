d6fe5b9dfb84d589b6ab42cc16132f660466d9dd
sneha shinde <Sneha06032000@gmail.com>
2022-07-25 19:04:59 +0530

    deleting object property

--- 25July.html/index.js
+++ 25July.html/index.js
@@ -10,10 +10,19 @@
 //   console.log(person.age)
 
 
+// let person = {
+//     firstname: "Sneha",
+//     lastname: "Shinde",
+//     age: 50,
+//   };
+  
+//   console.log(person["firstname"] + " is " + person["age"] + " years old.");
+
+
 let person = {
     firstname: "Sneha",
     lastname: "Shinde",
     age: 50,
   };
-  
-  console.log(person["firstname"] + " is " + person["age"] + " years old.");
+  delete person.lastname
+  console.log(person);
