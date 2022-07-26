bafd6023f85c54a6eee8294085b9752f0aa8dcf9
sneha shinde <Sneha06032000@gmail.com>
2022-07-26 19:06:22 +0530

    object destructuring

--- 26July.html/index.js
+++ 26July.html/index.js
@@ -1,3 +1,13 @@
-let nums=[1,2,3,4,5];
-let [num1,num2]=nums;
-console.log(num1,num2)
+// //destructuring
+// let nums=[1,2,3,4,5];
+// let [num1,num2]=nums;
+// console.log(num1,num2)
+
+
+//object destructuring
+let obj = {
+    name: "sneha shinde",
+    profession: "student in aircampus"
+};
+let { name, profession} = obj;
+console.log(name,profession)
