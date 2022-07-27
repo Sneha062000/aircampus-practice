9a2513126cdfaba2e32e1250584ac857dd7e83ed
sneha shinde <Sneha06032000@gmail.com>
2022-07-27 19:28:37 +0530

    replit ques

---
+++ 27July.html/index.js
@@ -0,0 +1,13 @@
+function commaSep(str){
+    let a = ""
+       for(i=0;i<str.length;i++){
+         a = a + "," +str[i]
+       }
+        return a.slice(1)
+        {
+        
+       //return str.split(" ").join(",");
+     }
+  }
+     console.log(commaSep('aircampus'))
+  
--- 30June.html/index.js
+++ 30June.html/index.js
@@ -1,30 +1,30 @@
-/*let n=5;
-for(let row=1;row<=n;row++){
-  let stars = '';
-  for(let col=1;col<=row;col++){
-    stars += ' '
-    stars  += col;
-  }
-  console.log(stars);
-}*/
+// let n=5;
+// for(let row=1;row<=n;row++){
+//   let stars = '';
+//   for(let col=1;col<=row;col++){
+//     stars += ' '
+//     stars  += col;
+//   }
+//   console.log(stars);
+// }
 
-/*let n=5;
-for(let row=1;row<=n;row++){
-  let stars = '';
-  for(let col=1;col<=row;col++){
-    stars += ' ';
-    stars  += row;
-  }
-  console.log(stars);
-}*/
+// let n=5;
+// for(let row=1;row<=n;row++){
+//   let stars = '';
+//   for(let col=1;col<=row;col++){
+//     stars += ' ';
+//     stars  += row;
+//   }
+//   console.log(stars);
+// }
 
-for(let row=5;row>=1;row--){
-  let stars = ''
-  for(let col=5;col>=row;col--){
-    stars += col
-  }
-  console.log(stars)
-}
+// for(let row=5;row>=1;row--){
+//   let stars = ''
+//   for(let col=5;col>=row;col--){
+//     stars += col
+//   }
+//   console.log(stars)
+// }
 
 // let n=5, count = 1;
 // let star = "";
