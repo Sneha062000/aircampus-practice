b98f3941231df71da025d8f8c2c082c4c4eeb3c9
sneha shinde <Sneha06032000@gmail.com>
2022-07-29 22:26:10 +0530

    min

--- 29July.html/index.js
+++ 29July.html/index.js
@@ -11,13 +11,26 @@
 
 
 
-let nums=[1,20,32,17,89,10,27,100]
+// let nums=[1,20,32,17,89,10,27,100]
 
-let max = 0;
-for(let i=0;i<nums.length;i++){
+// let max = 0;
+// for(let i=0;i<nums.length;i++){
 
-    if(max<nums[i]){
-        max = nums[i]
+//     if(max<nums[i]){
+//         max = nums[i]
+//     }
+// }
+// console.log(max)
+
+
+
+let nums=[-1,-20,-32,-17,-89,-10,-27,-100]
+
+let min = 0;
+for(let i=1;i-nums.length;i++){
+    let currentValue = nums[i]
+    if(min>currentValue){
+        min = currentValue
     }
 }
-console.log(max)
+console.log(min)
