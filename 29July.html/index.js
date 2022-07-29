bd79d97403b36d16c522288a1c4b5657a2098589
sneha shinde <Sneha06032000@gmail.com>
2022-07-29 19:29:54 +0530

    max

---
+++ 29July.html/index.js
@@ -0,0 +1,23 @@
+// let nums=[1,20,32,17,89,10,27,100]
+
+// let max = 0;
+// for(let i=0;i<nums.length;i++){
+//     let currentValue = nums[i]
+//     if(max<currentValue){
+//         max = currentValue
+//     }
+// }
+// console.log(max)
+
+
+
+let nums=[1,20,32,17,89,10,27,100]
+
+let max = 0;
+for(let i=0;i<nums.length;i++){
+
+    if(max<nums[i]){
+        max = nums[i]
+    }
+}
+console.log(max)
