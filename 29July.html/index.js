8eabbc1dbf424393ee40c28c0b3e9ea6a23de43b
sneha shinde <Sneha06032000@gmail.com>
2022-07-29 22:42:32 +0530

    sort

--- 29July.html/index.js
+++ 29July.html/index.js
@@ -24,13 +24,18 @@
 
 
 
-let nums=[-1,-20,-32,-17,-89,-10,-27,-100]
+// let nums=[-1,-20,-32,-17,-89,-10,-27,-100]
 
-let min = 0;
-for(let i=1;i-nums.length;i++){
-    let currentValue = nums[i]
-    if(min>currentValue){
-        min = currentValue
-    }
-}
-console.log(min)
+// let min = 0;
+// for(let i=1;i-nums.length;i++){
+//     let currentValue = nums[i]
+//     if(min>currentValue){
+//         min = currentValue
+//     }
+// }
+// console.log(min)
+
+
+let nums = [1,2,5,2,7,8,9]
+nums.sort()
+console.log(nums);
