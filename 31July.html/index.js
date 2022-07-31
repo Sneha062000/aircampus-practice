b468cc67edc7db58495acdb9ad8848bc3b376a72
sneha shinde <Sneha06032000@gmail.com>
2022-07-31 21:57:52 +0530

    factorial

---
+++ 31July.html/index.js
@@ -0,0 +1,23 @@
+// let age;
+// if(age<18){
+//     console.log("you can vote")
+// }
+// else{
+//     console.log("you can't vote")
+// }
+
+
+
+
+// for(let i =1;i<=100;i++){
+//     console.log(i)
+// }
+
+
+//factorial of 10
+let f=10;
+let fact=1;
+for(i=f; i>=1;i--){
+    fact=fact*i;
+}
+console.log("factorial of 10 is :"+ fact);
