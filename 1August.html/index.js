11df742f5a4e270e25a9cd96db1daabf73156c85
sneha shinde <Sneha06032000@gmail.com>
2022-08-01 17:51:33 +0530

    declaring variable

---
+++ 1August.html/index.js
@@ -0,0 +1,16 @@
+// //declaring variable
+// var a;
+// a=2;
+// console.log(a)
+
+// var a,b,c;
+// a=2;
+// b=3;
+// c=5;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var a=10;
+// b=a;
+// console.log(b);
