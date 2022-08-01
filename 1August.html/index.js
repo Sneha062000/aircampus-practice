bd95392572660c8a53182968012782b3834883c1
sneha shinde <Sneha06032000@gmail.com>
2022-08-01 18:08:45 +0530

    primitive data types

--- 1August.html/index.js
+++ 1August.html/index.js
@@ -14,3 +14,43 @@
 // var a=10;
 // b=a;
 // console.log(b);
+
+
+
+// //data types
+// //primitive data types
+
+
+// //numbers
+// let num = 10;
+// console.log(typeof num)
+
+
+// //string
+// let name="sneha123@";
+// console.log(typeof name)
+
+// let num=(1,2,3,"4")
+// console.log(typeof num)
+
+
+// //boolean
+// let num=true;
+// console.log(typeof num)
+
+// //undefined
+// let a;
+// console.log(a)
+
+
+// //null
+// let a=null;
+// console.log(a)
+
+// //symbol
+// let sym1 = Symbol("foo");
+// let sym2 = Symbol("foo")
+// console.log(sym1==sym2)
+// console.log(sym1===sym2)
+// //console.log(typeof sym1)
+// //console.log(typeof sym2)
