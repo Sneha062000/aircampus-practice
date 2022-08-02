3cf432a29cb7751f9e21bedf564744314de3f700
sneha shinde <Sneha06032000@gmail.com>
2022-08-02 18:49:03 +0530

    conditional statements

---
+++ 2August.html/index.js
@@ -0,0 +1,62 @@
+//conditional statements
+//syntax
+//if(condition){
+ //   block of code to executed
+//}
+
+
+//if statement
+// let age
+// if(age<18){
+//     console.log("you can vote")
+// }
+// console.log("you can't vote")
+
+
+//syntax
+//if(condition){
+ //   block of code to executed if the condition is true
+//}else{
+//block of code to be executed if the condition is false
+//}
+
+
+//if else statement
+// let a=10, b=20;
+// if(a>b){
+//     console.log("a is greater than b")
+// }else{
+//     console.log("b is greater than a")
+// }
+
+
+//syntax
+//if(condition){
+ //   block of code to executed if the condition is true
+//}else if(condition){
+//block of code to be executed if the condition is false
+//}else{
+//block of code to be executed if both the conditions is false
+//}
+
+// else if statement
+// let a=20, b=20;
+// if(a>b){
+//     console.log("a is greater than b")
+// }else if(b>a){
+//     console.log("b is greater than a")
+// }else{
+//     console.log("a is equals to b")
+// }
+
+//else if second example
+// let name="sneha"
+// if(name=="sneha"){
+//     console.log("my name is sneha")
+// }else if(name=="karishma"){
+//     console.log("my name is karishma")
+// }else if(name=="tejal"){
+//     console.log("my name is tejal")
+// }else{
+//     console.log("no match")
+// }
