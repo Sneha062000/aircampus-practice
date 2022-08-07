c725a9340f869ab094da9e5d44995b53e60a6933
sneha shinde <Sneha06032000@gmail.com>
2022-08-07 23:42:20 +0530

    object

---
+++ 7August.html/index.js
@@ -0,0 +1,28 @@
+// objects
+
+// let person = {
+//     firstName : "Sneha",
+//     lastName  : "shinde",
+//     age     : 50,
+
+//   };
+  
+//   console.log(person.age)
+
+
+// let person = {
+//     firstname: "Sneha",
+//     lastname: "Shinde",
+//     age: 50,
+//   };
+  
+//   console.log(person["firstname"] + " is " + person["age"] + " years old.");
+
+
+// let person = {
+//     firstname: "Sneha",
+//     lastname: "Shinde",
+//     age: 50,
+//   };
+//   delete person.lastname
+//   console.log(person);
