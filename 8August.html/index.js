dac919dc52c0c36ee1abb14f28e437eb0f17f6cd
sneha shinde <Sneha06032000@gmail.com>
2022-08-08 22:14:21 +0530

    recursion

---
+++ 8August.html/index.js
@@ -0,0 +1,36 @@
+//numbers from 1 to 5
+// function numbers(n){
+//     if(n > 5){
+//         return;
+//     }
+//     console.log(n);
+//     numbers(n + 1);
+// }
+
+// console.log(numbers(1));
+
+
+//number from 1 to n
+
+// function numbers(n,i=0){
+//     if(i<=n){
+//         console.log(i)
+//         return numbers(n,i+1)
+//     }
+// }
+
+// console.log(numbers(5))
+
+
+
+//number from 1 to n in reverse order
+
+// function numbers(n,i=1){
+//     if(i<=n){
+//         console.log(n)
+//         return numbers(n-1)
+//     }
+// }
+
+// console.log(numbers(5))
+
