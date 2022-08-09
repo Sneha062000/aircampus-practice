361c4a727712255bef69f62ddaa5024d60ab6714
sneha shinde <Sneha06032000@gmail.com>
2022-08-09 12:51:20 +0530

    every method

--- 8August.html/index.js
+++ 8August.html/index.js
@@ -36,11 +36,11 @@
 
 
 //factorial
-let factorial = (n)=>{
-    if(n===0){
-        return 1
-    }else{
-        return n*factorial(n-1)
-    }
-}
-console.log(factorial(5))
+// let factorial = (n)=>{
+//     if(n===0){
+//         return 1
+//     }else{
+//         return n*factorial(n-1)
+//     }
+// }
+// console.log(factorial(5))
---
+++ 9August .html/index.js
@@ -0,0 +1,14 @@
+// every
+
+//It returns a boolean value
+// let arr=[5,6,3,2,1,3,65,4,7,8,9,2,3,1]
+// let x=arr.every(function(num){
+//     return num>5
+// })
+// console.log(x)
+
+// let arr1=[6,7,8,9,6,7,8,9,10]
+// let y=arr1.every(function(num){
+//     return num>5
+// })
+// console.log(y)
