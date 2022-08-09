1207a1d2e853feda7038bd723d7945a6ac188fb6
sneha shinde <Sneha06032000@gmail.com>
2022-08-09 18:31:47 +0530

    closures

---
+++ 9August .html/index1.js
@@ -0,0 +1,12 @@
+//8 August class closures
+//callback function
+//higher order function
+
+var count = 1;
+function counter(){
+    count++;
+    console.log(count);
+}
+counter();
+counter();
+counter();
