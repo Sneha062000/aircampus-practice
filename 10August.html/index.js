bfb062173cc6cd8ff590e16decc89ba0f3d7fc03
sneha shinde <Sneha06032000@gmail.com>
2022-08-10 18:57:11 +0530

    anonymous function

--- 10August.html/index.js
+++ 10August.html/index.js
@@ -2,8 +2,14 @@
 //A callback is a function passed as an argument to another function
 
 
-const message = function() {  
-    console.log("This message is shown after 3 seconds");
-}
+// const message = function() {  
+//     console.log("This message is shown after 3 seconds");
+// }
  
-setTimeout(message, 3000);
+// setTimeout(message, 3000);
+
+//Anonymous Function
+//we can define a function directly inside another function, instead of calling it. 
+setTimeout(function() {  
+    console.log("This message is shown after 3 seconds");
+}, 3000);
