006759096c192d7501e647d6f0c38457247fbd46
sneha shinde <Sneha06032000@gmail.com>
2022-08-12 22:30:31 +0530

    class

---
+++ 12August.html/index.js
@@ -0,0 +1,25 @@
+//class
+//JavaScript Classes are templates for JavaScript Objects.
+//
+
+//Syntax
+// class ClassName {
+//     constructor() { ... }
+//   }
+
+
+// class car{
+//     constructor(model,color,brand,type){
+//         this.model=model;
+//         this.color=color;
+//         this.brand=brand;
+//         this.type=type;
+//     }
+//     run(){
+//         console.log("running");
+//     }
+//     playSongs(){
+//         console.log("songs");
+//     }
+// }
+// let hondaCity = new car("2022","red","honda","sedan")
