bf6fd9de23807d233d09822677cdab74ed214130
sneha shinde <Sneha06032000@gmail.com>
2022-08-13 22:34:28 +0530

    callback

---
+++ 13August.html/index.js
@@ -0,0 +1,8 @@
+//CallBack Function
+//A JavaScript callback is a function which is to be executed after another function has finished execution.
+
+function greet(name) {
+    console.log('Hi' + ' ' + name);
+}
+
+greet('sneha')
