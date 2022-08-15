6c2c7e846c5e6861bc4f82a3fdffcb3c5522313d
sneha shinde <Sneha06032000@gmail.com>
2022-08-15 21:15:43 +0530

    resolved

--- 15August/index.js
+++ 15August/index.js
@@ -8,20 +8,34 @@
 // 3 rejected: meaning that the operation failed.
 
 
-var promise = new Promise(function(resolve, reject) {
-    const x = "sneha";
-    const y = "sneha"
-    if(x === y) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+// var promise = new Promise(function(resolve, reject) {
+//     const x = "sneha";
+//     const y = "sneha"
+//     if(x === y) {
+//       resolve();
+//     } else {
+//       reject();
+//     }
+//   });
      
-  promise.
-      then(function () {
-          console.log('Success, Hello sneha shinde');
-      }).
-      catch(function () {
-          console.log('Some error has occurred');
-      });
+//   promise.
+//       then(function () {
+//           console.log('Success, Hello sneha shinde');
+//       }).
+//       catch(function () {
+//           console.log('Some error has occurred');
+//       });
+
+
+// //Promise Resolved
+// var promise = new Promise(function(resolve, reject) {
+//     resolve('sneha');
+// })
+   
+// promise
+//     .then(function(successMessage) {
+//        //success handler function is invoked
+//         console.log(successMessage);
+//     }, function(errorMessage) {
+//         console.log(errorMessage);
+//     })
