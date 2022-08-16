ec642d6df414558a8a9d3f2cab9c506e2e4cdad3
sneha shinde <Sneha06032000@gmail.com>
2022-08-16 22:22:08 +0530

    practice

---
+++ 16August/index.js
@@ -0,0 +1,51 @@
+
+
+// Person();
+// class Person1 {
+//   constructor() {
+//     console.log("person in class");
+//   }
+// }
+
+// new Person1();
+
+// function Person() {
+//   console.log("person in fn ");
+// }
+
+
+
+// function declarations are hoisted but classes are not hoisted
+
+// class -> it is a blue print
+// objects -> instance of class or real time entity
+
+// class Human {
+//   constructor(name, age, gender) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+
+//   eat() {
+//     console.log("eating ");
+//   }
+
+//   sleep() {
+//     console.log("sleeping");
+//   }
+
+//   think() {
+//      console.log("thinking");
+//     return "thinking";
+//   }
+// }
+
+// let sneha = new Human("sneha", 10, "Female");
+// console.log(sneha, sneha.think());
+
+
+
+
+
+
