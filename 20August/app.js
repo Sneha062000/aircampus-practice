c9c1b1b9f13ba98c853bb652e89aa18edd5a94d7
sneha shinde <Sneha06032000@gmail.com>
2022-08-20 21:42:31 +0530

    classes

---
+++ 20August/app.js
@@ -0,0 +1,36 @@
+//classes
+//Classes are templates for JavaScript Objects.
+
+//syntax
+// class ClassName {
+//     constructor() { ... }
+// }
+
+// class MyName {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+//     }
+//   }
+  
+//   const myName = new MyName("Sneha", 2022);
+//   console.log( myName.name + " " + myName.year);
+
+
+
+// class MyName {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+  
+//     }
+//     age() {
+//       let date = new Date();
+//       return date.getFullYear() - this.year;
+//     }
+//   }
+  
+//   let myName = new MyName("sneha", 2012);
+//   console.log("My Name is " + myName.age() + " years old.")
+  
+  
