170def35013da65827e4a531f2d5661b61e69906
sneha shinde <Sneha06032000@gmail.com>
2022-08-21 19:10:09 +0530

    this

---
+++ 21August/index.js
@@ -0,0 +1,34 @@
+//this Keyword
+//In JavaScript, the this keyword refers to an object.
+//In an object method, this refers to the object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+
+
+
+// const person = {
+//     firstName: "Sneha",
+//     middleName:"Manoj",
+//     lastName: "Shinde",
+//     fullName : function() {
+//       return this.firstName + " " + this.middleName +" "+this.lastName;
+//     }
+//   };
+//   console.log(person.fullName());
+
+
+
+// let x = this;
+// console.log("hello")
+
+
+
+
+
+// console.log(myFunction());
+
+// function myFunction() {
+//   return this;
+// }
