1716ad000b12ecdd0081bd98e5384026469bcb04
sneha shinde <Sneha06032000@gmail.com>
2022-08-24 19:02:00 +0530

    objects practice

---
+++ 24August/objects.js
@@ -0,0 +1,22 @@
+//objects
+//In JavaScript, an object is a standalone entity, with properties and type
+
+// Booleans can be objects 
+// Numbers can be objects 
+// Strings can be objects 
+// Dates are always objects
+// Maths are always objects
+// Regular expressions are always objects
+// Arrays are always objects
+// Functions are always objects
+// Objects are always objects
+
+
+// let person = {
+//     firstName : "snehs",
+//     lastName  : "shinde",
+//     age     : 100,
+//     place  : "Nashik"
+//   };
+  
+//   console.log(person.firstName + " " + person.lastName + " " + person.age + " " + person.place);
