aa2d9557e451281e8544def924419f1dd77a91a9
sneha shinde <Sneha06032000@gmail.com>
2022-08-24 18:45:51 +0530

    functions practice

---
+++ 24August/functions.js
@@ -0,0 +1,48 @@
+//Functions
+//A JavaScript function is a block of code designed to perform a particular task.
+//A JavaScript function is executed when "something" invokes it (calls it).
+
+
+// function myFunction(a, b) {
+//     return a + b;
+//   }
+//   console.log(myFunction(5, 5));
+
+
+
+
+//This example calls a function which performs a calculation and returns the result:
+// var x = myFunction(5, 5);
+// console.log(x);
+
+// function myFunction(a, b) {
+//   return a + b;
+// }
+
+
+
+
+//This example calls a function to convert from Fahrenheit to Celsius:
+// function toCelsius(f) {
+//     return (5/9) * (f-32);
+//   }
+//   console.log(toCelsius(88));
+
+
+
+
+//Accessing a function without () will return the function definition instead of the function result:
+// function toCelsius(f) {
+//     return (5/9) * (f-32);
+//   }
+//   console.log(toCelsius);
+
+
+
+// myFunction();
+
+// function myFunction() {
+//   let Name = "Sneha";
+//   let lastName = "Shinde"
+//   console.log(typeof Name + " " + Name + " " + lastName);
+// }
