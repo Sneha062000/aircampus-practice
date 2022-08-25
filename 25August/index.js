4e63d460fc059e9d392e1b84333e0b2f820b06b2
sneha shinde <Sneha06032000@gmail.com>
2022-08-25 17:58:34 +0530

    javasxript practice

---
+++ 25August/index.js
@@ -0,0 +1,55 @@
+//loops
+
+
+//for loop
+//syntax
+//for([counter initialization];[condition];counter update){
+  //  statement to be executed
+//}
+
+// for(let i=1;i<=9;i++){
+//     console.log(i)
+// }
+
+
+
+//syntax
+//while(condition){
+//block of code to be executed
+//}
+
+//while
+// let i=0;
+// while(i<=9){
+//     console.log(i)
+//     i++;
+// }
+
+
+//do{
+//    code to be executed
+//}
+//while(condition)
+
+// let i=1;
+// do{
+//     console.log(i)
+//     i++;
+// }
+// while(i<=9)
+
+
+// let arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+// let n = arr.length
+
+// for(let i=0; i<n; i++){
+
+//     let newArray = arr[i]
+//     console.log("newArray" , newArray)
+//     for(let j=0; j<newArray.length; j++){
+//         console.log(newArray[j]);
+//     }
+
+// }
+
