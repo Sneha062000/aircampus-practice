44f78cf01aa941dd7228ebb9d5727b5280da681e
sneha shinde <Sneha06032000@gmail.com>
2022-08-29 19:09:49 +0530

    apply

---
+++ 29August/apply.js
@@ -0,0 +1,32 @@
+// const person = {
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   const person1 = {
+//     firstName:"sneha",
+//     lastName: "shinde"
+//   }
+
+//   const person2 = {
+//     firstName:"Rajesh",
+//     lastName: "shinde"
+//   }
+  
+//   console.log(person.fullName.apply(person2)); 
+
+
+
+// const person = {
+//     fullName: function(city, country) {
+//       return this.firstName + " " + this.lastName + "," + city + "," + country;
+//     }
+//   }
+  
+//   const person1 = {
+//     firstName:"Sneha",
+//     lastName: "Shinde"
+//   }
+  
+//   console.log(person.fullName.apply(person1, ["Nashik", "India"])); 
