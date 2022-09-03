6e51573e14e98eb6a3c6c7f48f6707505bba87af
sneha shinde <Sneha06032000@gmail.com>
2022-09-03 23:37:26 +0530

    practice

---
+++ 2 Sep/index.js
@@ -0,0 +1,13 @@
+//Hoisting
+//Hoisting is JavaScript's default behavior of moving declarations to the top.
+
+// var a = 4;
+
+// function names() {
+//     b = 'hello';
+//     console.log(b); 
+//     var b;
+// }
+
+// names();
+// console.log(b);
