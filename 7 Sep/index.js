4fec0f27a76c563993057508a6f07cc2ba953a79
sneha shinde <Sneha06032000@gmail.com>
2022-09-07 19:17:57 +0530

    practice

---
+++ 7 Sep/index.js
@@ -0,0 +1,12 @@
+// for(i=1;i<=60;i++){
+//     console.log(i)
+// }
+
+// for(i=6;i<=60;i+=6){
+//     console.log(i)
+// }
+
+// var i = 5;
+// for (var i = 0; i < 10; i++) {
+// }
+// console.log(i);
