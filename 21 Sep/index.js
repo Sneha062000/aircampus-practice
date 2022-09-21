a60b3b811fdd380269e9775f9cf2cb4ab87fa9cc
sneha shinde <Sneha06032000@gmail.com>
2022-09-21 19:08:48 +0530

    practice

---
+++ 21 Sep/index.js
@@ -0,0 +1,45 @@
+// myFunction();
+
+// function myFunction() {
+//   let a = 5;
+//   console.log(a * a);
+// }
+
+
+// let counter = 0;
+
+// // Function to increment counter
+// function add() {
+//   counter += 1;
+// }
+
+// // Call add() 6 times
+// add();
+// add();
+// add();
+// add();
+// add();
+// add();
+
+// // The counter should now be 6
+// console.log("The counter is: " + counter);
+
+
+
+let counter = 10;
+
+// Function to decrement counter
+function add() {
+  counter -= 1;
+}
+
+// Call sub() 6 times
+add();
+add();
+add();
+add();
+add();
+add();
+
+// The counter should now be 4
+console.log("The counter is: " + counter);
