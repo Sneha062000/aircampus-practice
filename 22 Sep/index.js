1bb11b31815eccb55cbf174483913f72a236b08d
sneha shinde <Sneha06032000@gmail.com>
2022-09-22 19:07:56 +0530

    practice

---
+++ 22 Sep/index.js
@@ -0,0 +1,32 @@
+// const person = {
+//   fullName: function() {
+//     return this.firstName + " " + this.middleName + " " + this.lastName;
+//   }
+// }
+
+// const person1 = {
+//   firstName:"sneha",
+//   middleName:"manoj",
+//   lastName: "shinde"
+// }
+
+// console.log(person.fullName.apply(person1));
+
+
+// const person = {
+//   fullName: function(city, country) {
+//     return this.firstName + " " + this.lastName + "," + city + "," + country;
+//   }
+// }
+
+// const person1 = {
+//   firstName:"sneha",
+//   lastName: "shinde"
+// }
+
+// const person2 = {
+//   firstName:"sanjana",
+//   lastName: "shinde"
+// }
+
+// console.log(person.fullName.call(person2, "nashik", "India")); 
