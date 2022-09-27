7f2c9ef5c7da8468b7b51fdbe77bca44da1ed8c0
sneha shinde <Sneha06032000@gmail.com>
2022-09-27 22:29:40 +0530

    practice

---
+++ 27 Sep/index.js
@@ -0,0 +1,13 @@
+// const letters = new Set(["a", "b", "c", "d"]);
+// console.log(letters.size);
+
+
+const letters = new Set();
+
+// Add Values to the Set
+letters.add("abc");
+letters.add("b");
+letters.add("c");
+
+// Display set.size
+console.log(letters.size);
