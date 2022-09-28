0d04becb22d27ec06439b9d1c0350dc20e8af52b
sneha shinde <Sneha06032000@gmail.com>
2022-09-28 19:13:33 +0530

    practice

---
+++ 28 Sep/index.js
@@ -0,0 +1,104 @@
+//array
+
+// let arr=[1,2,3,4,5];
+// let a=arr.length;
+// console.log(a);
+
+// push
+
+// arr.push("sneha")
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// unshift-
+
+// arr.unshift("sneha")
+// console.log(arr)
+
+// shift
+
+// arr.shift()
+// console.log(arr)
+
+// Includes(value)
+
+// let x=arr.includes(10)
+// console.log(x);
+
+// Includes(value,n)
+
+// let x=arr.includes(10,2)
+// console.log(x);
+
+// indexOf(value)
+
+// let x=arr.indexOf(10)
+// console.log(x)
+
+// indexOf(value,n)
+// let x=arr.indexOf(15,5)
+// console.log(x)
+
+// let x=arr.lastIndexOf(15,5)
+// console.log(x)
+
+// Reverse
+
+// let x=arr.reverse()
+// console.log(x)
+
+// join
+
+// let x=arr.toString('/')
+// console.log(x)
+
+// splice
+
+// let x=arr.splice(1,6,20)
+// console.log(x)
+// console.log(arr)
+
+// filter
+
+// let arr1=[2,11,4,5,6,99,100]
+
+// let x=arr1.filter((num)=>{
+//     return num>3
+// })
+// console.log(x)
+// console.log(arr1)
+
+// let y=arr1.filter((num)=>{
+//     return num%2==0
+// })
+// console.log(y);
+
+// every
+
+// let arr=[5,6,3,2,1,3,65,4,7,8,9,2,3,1]
+// let x=arr.every(function(num){
+//     return num>5
+// })
+// console.log(x)
+
+// let arr1=[6,7,8,9,6,7,8,9,10]
+// let y=arr1.every(function(num){
+//     return num>5
+// })
+// console.log(y)
+
+// some
+
+// let arr=[5,6,3,2]
+// let x=arr.some(function(num){
+//     return num>5
+// })
+// console.log(x)
+
+// let arr1=[1,2,3,4,5,1,2,3]
+// let y=arr1.every(function(num){
+//     return num>5
+// })
+// console.log(y)
