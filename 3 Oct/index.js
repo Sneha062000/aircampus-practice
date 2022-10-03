c958dcfe0d4ce3a46aeec9fb31a473c3ca973903
sneha shinde <Sneha06032000@gmail.com>
2022-10-03 19:21:10 +0530

    practice

---
+++ 3 Oct/index.js
@@ -0,0 +1,25 @@
+// function triple(n) {
+//   return n * 3;
+// }
+// arr = new Array(1, 2, 3, 6, 5, 4);
+
+// var new_arr = arr.map(triple);
+// console.log(new_arr);
+
+
+
+// function product(a, b) {
+//   return a * b;
+// }
+// arr = new Array(1, 2, 3, 6, 5, 4);
+
+// var product_of_arr = arr.reduce(product);
+// console.log(product_of_arr);
+
+
+arr = new Array(1, 2, 3, 6, 5, 4);
+var new_arr = arr.filter(function (x) {
+  return x % 2 == 0;
+});
+
+console.log(new_arr);
