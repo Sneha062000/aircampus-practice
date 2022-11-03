9331e3d2574087bb03b5bb9f5ff9bc24f8c51b8b
sneha shinde <Sneha06032000@gmail.com>
2022-11-03 22:27:36 +0530

    hoisting

---
+++ 1 Nov/index.js
@@ -0,0 +1,36 @@
+//Hoisting
+
+//  console.log(a)
+// var a=10;
+
+
+// var a;
+// console.log(a);
+
+
+// a = 5;
+// console.log(a);
+// var a; 
+
+
+// a = 5;
+// console.log(a);
+// let a;
+
+// var a;
+// a = 5;
+// console.log(a);
+
+
+// names();
+
+// function names() {
+//   console.log("sneha");
+// }
+
+
+// names();
+
+// let names = function () {
+//   console.log("sneha");
+// };
