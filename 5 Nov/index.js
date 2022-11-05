0ef45b189541300de6faf11d98ef93b596433694
sneha shinde <Sneha06032000@gmail.com>
2022-11-05 21:59:09 +0530

    throttling and debouncing

---
+++ 5 Nov/index.js
@@ -0,0 +1,10 @@
+// Debouncing & Throttling
+// Debouncing and throttling are techniques in javascript to optimise the application and browser performance.
+
+//What is a debounce function?
+
+//Debounce function limits the execution of a function call and waits for a certain amount of time before running it again.
+
+//What is throttling.
+//Throttling or sometimes is also called throttle function is a practice used in websites. Throttling is used to call a 
+// function after every millisecond or a particular interval of time only the first click is executed immediately.
