877e8fc3a8900036d8a808cffb2d857589e396c1
sneha shinde <Sneha06032000@gmail.com>
2022-11-25 22:03:41 +0530

    practice

---
+++ 25 Nov/index1.js
@@ -0,0 +1,39 @@
+// var car = new Vehicle("Honda", "white", "2010", "UK");
+// console.log(car);
+
+// function Vehicle(model, color, year, country) {
+//   this.model = model;
+//   this.color = color;
+//   this.year = year;
+//   this.country = country;
+// }
+
+
+// function foo() {
+//   let x = (y = 0);
+//   x++;
+//   y++;
+//   return x;
+// }
+
+// console.log(foo(), typeof x, typeof y);
+
+
+// function main() {
+//   console.log("A");
+//   setTimeout(function print() {
+//     console.log("B");
+//   }, 0);
+//   console.log("C");
+// }
+// main();
+
+
+// console.log(0.1 + 0.2 === 0.3);
+
+
+// var y = 1;
+// if (function f() {}) {
+//   y += typeof f;
+// }
+// console.log(y);
