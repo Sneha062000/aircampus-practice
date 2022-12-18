c76bef4f57b44133483a14e62b0d882909958a54
sneha shinde <Sneha06032000@gmail.com>
2022-12-18 21:59:31 +0530

    practice

---
+++ 18 Dec/index.js
@@ -0,0 +1,196 @@
+//declaring variable
+// var a;
+// a=2;
+// console.log(a)
+
+// var a,b,c;
+// a=2;
+// b=3;
+// c=5;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var a=10;
+// b=a;
+// console.log(b);
+
+// //data types
+// //primitive data types
+
+// //numbers
+// let num = 10;
+// console.log(typeof num)
+
+// //string
+// let name="sneha123@";
+// console.log(typeof name)
+
+// let num=(1,2,3,"4")
+// console.log(typeof num)
+
+// //boolean
+// let num=true;
+// console.log(typeof num)
+
+// //undefined
+// let a;
+// console.log(a)
+
+// //null
+// let a=null;
+// console.log(a)
+
+// //symbol
+// let sym1 = Symbol("foo");
+// let sym2 = Symbol("foo")
+// console.log(sym1==sym2)
+// console.log(sym1===sym2)
+// //console.log(typeof sym1)
+// //console.log(typeof sym2)
+
+// Non primitive data types objects
+// syntax:
+// let object_name={
+//     key1:value1,
+//     key2:value2
+// }
+
+//object
+// let student ={
+//     name:"sneha",
+//     experience:"fresher",
+//     age:100,
+//     declaration:true,
+//     languages_known: "",
+// }
+// //console.log(student)
+// console.log(student.name)
+// console.log(student["name"])
+
+// //nested object
+// //inside another object is called nested object
+// let student ={
+//     name:"sneha",
+//     experience:"fresher",
+//     age:100,
+//     declaration:true,
+//     language:{
+//        html:"70%",
+//        css:"60%",
+//        javascript:"10%"
+//     }
+// }
+// console.log(student)
+
+//Arithmetic operators
+//add
+// let a=10,b=5;
+// let c=a+b;
+// console.log(c)
+
+//sub
+// let a=10,b=5;
+// let c=a-b;
+// console.log(c)
+
+//multiply
+// let a=10,b=5;
+// let c=a*b;
+// console.log(c)
+
+//divide
+// let a=10,b=5;
+// let c=a/b;
+// console.log(c)
+
+//modules
+// let a=10,b=5;
+// let c=a%b;
+// console.log(c)
+
+//Increment operator
+//  let a=10,b=5;
+// a++;
+// b++
+// console.log(a,b)
+
+// //Decrement operator
+//  let a=10,b=5;
+// a--;
+// b--;
+// console.log(a,b)
+
+// comparision operator
+// equality operator
+// let a="10",b=10;
+// console.log(a==b)
+
+//strict equality operator
+//===  compares equality of two operands with type
+// let a="10",b=10;
+// console.log(a===b)
+
+//not equal operator
+// let a=10,b=10;
+// console.log(a!=b)
+
+//Greater than operator
+// let a=10,b=5;
+// console.log(a>b)
+
+//less than operator
+// let a=10,b=5;
+// console.log(a<b)
+
+//greater than or equal than
+// let a=10,b=5;
+// console.log(a<=b)
+
+//logical operators
+//and operator
+// let a=10,b=5;
+// console.log((a>b)&&(a>b))
+
+//or operator
+// let a=10,b=5;
+//  console.log((a>b)||(a=b))
+
+//not operator
+// let a=10,b=10;
+// console.log(!(a==b))
+
+//Assignment operator
+// let a=10;
+// let b=a;
+// console.log(b);
+
+//addition assignment operotator
+// let a=10;
+// a+=1
+// console.log(a)
+
+//substraction assignment operotator
+// let a=10;
+// a-=1
+// console.log(a)
+
+//multiplication assignment operotator
+// let a=10;
+// a*=2
+// console.log(a)
+
+//Division assignment operotator
+// let a=4;
+// a/=6
+// console.log(a)
+
+//Modulo assignment operotator
+// let a=2;
+// a%=5
+// console.log(a)
+
+//Ternary operator
+// let a = 10;
+// let b = 5;
+// (a>b) ? console.log("a is greater than b"):console.log("b is greater than a")
