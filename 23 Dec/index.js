6c747e7737a96358f872942dda049ccb1e3928d2
sneha shinde <Sneha06032000@gmail.com>
2022-12-26 22:19:58 +0530

    practice

--- 23 Dec/index.js
+++ 23 Dec/index.js
@@ -76,29 +76,28 @@
 
 
 
-// Example 3: Use clearInterval() Method
+// // Example 3: Use clearInterval() Method
 
-// program to stop the setInterval() method after five times
+// // program to stop the setInterval() method after five times
 
-let count = 0;
+// let count = 0;
 
-// function creation
-let interval = setInterval(function(){
+// // function creation
+// let interval = setInterval(function(){
 
-    // increasing the count by 1
-    count += 1;
+//     // increasing the count by 1
+//     count += 1;
 
-    // when count equals to 5, stop the function
-    if(count === 5){
-        clearInterval(interval);
-    }
+//     // when count equals to 5, stop the function
+//     if(count === 5){
+//         clearInterval(interval);
+//     }
 
-    // display the current time
-    let dateTime= new Date();
-    let time = dateTime.toLocaleTimeString();
-    console.log(time);
+//     // display the current time
+//     let dateTime= new Date();
+//     let time = dateTime.toLocaleTimeString();
+//     console.log(time);
 
-}, 2000);
-
+// }, 2000);
 
 
---
+++ 26 Dec/index.js
@@ -0,0 +1,26 @@
+// function test(arg1, arg2) {
+//   console.log(this.num, arg1, arg2); // 100, 10, 20
+// }
+
+// test.call({ num: 100 }, 10, 20);
+
+
+
+// function test(...arguments) {
+//   console.log(this.num, arguments); //100, [1,2,3]
+// }
+
+// test.apply({ num: 100 }, [1, 2, 3]); 
+
+
+// function test(arg){
+//  console.log(this.number, arg);
+// }
+
+// let bindedFn = test.bind({number: 99}, "argument");
+
+// bindedFn(); // 99, "argument"
+
+
+
+
