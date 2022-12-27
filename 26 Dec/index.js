3818fa89c23bb7691e47b49f981155683d039364
sneha shinde <Sneha06032000@gmail.com>
2022-12-27 12:32:46 +0530

    practice

--- 26 Dec/index.js
+++ 26 Dec/index.js
@@ -24,3 +24,80 @@
 
 
 
+// const person = {
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+// const person1 = {
+//   firstName: "John",
+//   lastName: "Doe",
+// };
+// const person2 = {
+//   firstName: "Mary",
+//   lastName: "Doe",
+// };
+// console.log(person.fullName.call(person1)); 
+
+
+
+// const person = {
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+
+// const person1 = {
+//   firstName: "John",
+//   lastName: "Doe",
+// };
+
+// console.log(person.fullName.apply(person1));
+
+
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+
+// const member = {
+//   firstName: "Hege",
+//   lastName: "Nilsen",
+// };
+
+// let fullName = person.fullName.bind(member);
+
+// console.log(fullName())
+
+
+
+const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter;}
+})();
+function myFunction(){
+  console.log(add())
+}
+
+
+
+let myPromise = new Promise(function (myResolve, myReject) {
+  // "Producing Code" (May take some time)
+
+  myResolve(); // when successful
+  myReject(); // when error
+});
+
+// "Consuming Code" (Must wait for a fulfilled Promise)
+myPromise.then(
+  function (value) {
+    /* code if successful */
+  },
+  function (error) {
+    /* code if some error */
+  }
+);
