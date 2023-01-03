1df1f722b3240b772470c66d058f5260a2b2e1e8
sneha shinde <Sneha06032000@gmail.com>
2023-01-03 22:32:20 +0530

    practice

---
+++ 3 Jan/index.js
@@ -0,0 +1,20 @@
+// function myDisplayer(something) {
+//   console.log(something);
+// }
+
+// function myCalculator(num1, num2, myCallback) {
+//   let sum = num1 + num2;
+//   myCallback(sum);
+// }
+
+// myCalculator(5, 5, myDisplayer);
+
+
+
+setTimeout(function () {
+  myFunction("Hello Sneha Shinde !!!");
+}, 3000);
+
+function myFunction(value) {
+  console.log(value);
+}
