f55e164e121a285e98c1848c23fe0217e16daa35
sneha shinde <Sneha06032000@gmail.com>
2023-01-05 21:59:19 +0530

    practice

---
+++ 5 Jan/index.js
@@ -0,0 +1,31 @@
+// const fullname = new Map([
+//   ["sneha", 500],
+//   ["manoj", 300],
+//   ["shinde", 200],
+// ]);
+
+// console.log(fullname.get("sneha"));
+
+
+// // Delete an Element
+// const fullname = new Map([
+//   ["sneha", 500],
+//   ["manoj", 300],
+//   ["shinde", 200],
+// ]);
+
+
+// fullname.delete("sneha");
+
+// console.log(fullname.size);
+
+
+// // Clear the Map
+// const fullname = new Map([
+//   ["sneha", 500],
+//   ["manoj", 300],
+//   ["shinde", 200],
+// ]);
+// fullname.clear();
+
+// console.log(fullname.size);
