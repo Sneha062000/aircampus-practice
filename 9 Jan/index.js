94dff800162dc5065c40029e7ef924678ee9c18e
sneha shinde <Sneha06032000@gmail.com>
2023-01-09 19:42:57 +0530

    practice

---
+++ 9 Jan/index.js
@@ -0,0 +1,54 @@
+// objects
+
+// let person = {
+//     firstName : "Sneha",
+//     lastName  : "shinde",
+//     age     : 50,
+
+//   };
+
+//   console.log(person.age)
+
+// let person = {
+//     firstname: "Sneha",
+//     lastname: "Shinde",
+//     age: 50,
+//   };
+
+//   console.log(person["firstname"] + " is " + person["age"] + " years old.");
+
+// let person = {
+//     firstname: "Sneha",
+//     lastname: "Shinde",
+//     age: 50,
+//   };
+//   delete person.lastname
+//   console.log(person);
+
+
+
+//class
+//JavaScript Classes are templates for JavaScript Objects.
+//
+
+//Syntax
+// class ClassName {
+//     constructor() { ... }
+//   }
+
+
+// class car{
+//     constructor(model,color,brand,type){
+//         this.model=model;
+//         this.color=color;
+//         this.brand=brand;
+//         this.type=type;
+//     }
+//     run(){
+//         console.log("running");
+//     }
+//     playSongs(){
+//         console.log("songs");
+//     }
+// }
+// let hondaCity = new car("2022","red","honda","sedan")
