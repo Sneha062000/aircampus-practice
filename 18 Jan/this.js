7bf88b4fc8ee8a4302a230e9e35a6da40dc6195a
sneha shinde <Sneha06032000@gmail.com>
2023-01-18 21:53:19 +0530

    practice

---
+++ 18 Jan/this.js
@@ -0,0 +1,40 @@
+// this
+
+// const video = {
+//     a:"10",
+//     b:20,
+//     play(){
+//         console.log(this)
+//     }
+// };
+// function playVideo(){
+//     console.log(this)
+// }
+
+
+// playVideo();
+
+
+// const person = {
+//  firstName:"sneha",
+//  lastName:"shinde",
+
+//  fullName:function(){
+//     return this.firstName + " " + this.lastName;
+//  }
+// }
+
+// console.log(person.fullName())
+// o/p:sneha shinde
+
+// const person = {
+//   firstName: "sneha",
+//   lastName: "shinde",
+
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+
+// console.log(person);  
+// o/p:object
