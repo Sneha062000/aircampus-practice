b5727c35b705ecf2b11e3bd59a7016fce6ea70b1
sneha shinde <Sneha06032000@gmail.com>
2023-01-24 09:58:40 +0530

    practice

---
+++ 18 Jan/promise.js
@@ -0,0 +1,18 @@
+let myPromise = new Promise(function (myResolve, myReject) {
+  let x = 0;
+
+  // some code (try to change x to 5)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function (value) {(value);
+  },
+  function (error) {(error);
+  }
+);
