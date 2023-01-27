f7775b8af9fd7560cacadb0fc17563a0e75d5f8d
sneha shinde <Sneha06032000@gmail.com>
2023-01-27 22:50:42 +0530

    practice

--- 18 Jan/promise.js
+++ 18 Jan/promise.js
@@ -1,18 +1,32 @@
-let myPromise = new Promise(function (myResolve, myReject) {
-  let x = 0;
+// let myPromise = new Promise(function (myResolve, myReject) {
+//   let x = 0;
 
-  // some code (try to change x to 5)
+//   // some code (try to change x to 5)
 
-  if (x == 0) {
-    myResolve("OK");
-  } else {
-    myReject("Error");
+//   if (x == 0) {
+//     myResolve("OK");
+//   } else {
+//     myReject("Error");
+//   }
+// });
+
+// myPromise.then(
+//   function (value) {(value);
+//   },
+//   function (error) {(error);
+//   }
+// );
+
+let cars = new Promise(function(resolve,reject){
+  let fuel_tank=true;
+  if(fuel_tank){
+    resolve();
+  }else{
+    reject();
   }
 });
-
-myPromise.then(
-  function (value) {(value);
-  },
-  function (error) {(error);
-  }
-);
+cars.then(function(){
+  console.log("fuel_tank is full")
+}).catch(function(){
+  console.log("error")
+})
